430e2e3246001907941b61575085c0e538b2d783
Sahan Takhachhen <takhachhensahan@gmail.com>
2025-12-31 12:08:37 +0545

    install redis

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class SampleModel(models.Model):
+    name = models.CharField(max_length=100)
+    
+    
+class User(AbstractUser):
+    pass
+    
