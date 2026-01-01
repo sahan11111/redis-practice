2c68c762fd655aeedac455e4e0132bab9a857bf2
Sahan Takhachhen <takhachhensahan@gmail.com>
2026-01-01 14:02:14 +0545

    use default router

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router=DefaultRouter()
+router.register(r'users',views.UserViewSet,basename='user')
 
 urlpatterns = [
     path('sample/', views.SampleModelViewSet.as_view({'get': 'list', 'post': 'create'}), name='sample-model'),
-    path('users/', views.UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list-create'),
+    path('', include(router.urls)),
 ]
--- app/views.py
+++ app/views.py
@@ -34,43 +34,43 @@ class UserViewSet(ModelViewSet):
         return Response(data)
 
 
-class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = seralizers.UserSerializer
-    permission_classes = [AllowAny]
-
-    def list(self, request, *args, **kwargs):
-        cache_key = "users_list_v1"
-        data = cache.get(cache_key)
-
-        if data is None:
-            queryset = self.get_queryset()
-            serializer = self.get_serializer(queryset, many=True)
-            data = serializer.data
-            cache.set(cache_key, data, timeout=300)  # 5 minutes
-
-        return Response(data)
-    
 # class UserViewSet(ModelViewSet):
 #     queryset = User.objects.all()
 #     serializer_class = seralizers.UserSerializer
 #     permission_classes = [AllowAny]
 
-#     CACHE_KEY = "users_list_v1"
-#     CACHE_TTL = 100  
+    # def list(self, request, *args, **kwargs):
+    #     cache_key = "users_list_v1"
+    #     data = cache.get(cache_key)
 
-#     def list(self, request, *args, **kwargs):
-#         data = cache.get(self.CACHE_KEY)
+    #     if data is None:
+    #         queryset = self.get_queryset()
+    #         serializer = self.get_serializer(queryset, many=True)
+    #         data = serializer.data
+    #         cache.set(cache_key, data, timeout=300)  # 5 minutes
 
-#         if data is None:
-#             print("🔥 DATABASE HIT")
-#             serializer = self.get_serializer(self.get_queryset(), many=True)
-#             data = serializer.data
-#             cache.set(self.CACHE_KEY, data, self.CACHE_TTL)
-#         else:
-#             print("⚡ CACHE HIT")
+    #     return Response(data)
+    
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = seralizers.UserSerializer
+    permission_classes = [AllowAny]
 
-#         return Response(data)
+    CACHE_KEY = "users_list_v1"
+    CACHE_TTL = 100  
+
+    def list(self, request, *args, **kwargs):
+        data = cache.get(self.CACHE_KEY)
+
+        if data is None:
+            print("🔥 DATABASE HIT")
+            serializer = self.get_serializer(self.get_queryset(), many=True)
+            data = serializer.data
+            cache.set(self.CACHE_KEY, data, self.CACHE_TTL)
+        else:
+            print("⚡ CACHE HIT")
+
+        return Response(data)
 
 
         
